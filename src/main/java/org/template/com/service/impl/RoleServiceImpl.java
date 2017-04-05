ca8c324e0fbe54c6ec3bf85f5210ef4edd956746
王瑞涛 <wangruitao@midaigroup.com>
2017-04-05 10:29:46 +0800

    bug

--- src/main/java/org/template/com/service/impl/RoleServiceImpl.java
+++ src/main/java/org/template/com/service/impl/RoleServiceImpl.java
@@ -35,7 +35,7 @@ public class RoleServiceImpl implements RoleService{
 	@Override
 	public Set<Role> loadRolesByLoginName(String userName) {
 		
-		String sql = "select r.id, r.role_name roleName , r.role_code roleCode, r.remarker, r.disabled, r.createTime createTime , "
+		String sql = "select r.id, r.role_name roleName , r.role_code roleCode, r.remarker, r.disabled, r.create_time createTime , "
 				+ " r.update_time update_time  FROM t_role r inner join t_user_role ru on r.id=ru.role_id  "
 				+ "inner join t_user u on ru.user_id = u.id where u.login_name=#{userName} and r.disabled=0";
 		List<Role> roles = sqlMapper.selectList(sql, userName, Role.class);
